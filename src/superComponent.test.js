1ecda36fef7c4ebae5098bfc2c5b4378ce57e54f
Gregory Caloin <gregchapauli@gmail.com>
2022-07-07 16:08:03 +0200

    test "affichage du texte"

--- src/superComponent.test.js
+++ src/superComponent.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import SuperComponent from "./superComponent";
 
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/greg/i);
+  render(<SuperComponent />);
+  const linkElement = screen.getByText(/Mon super composant/i);
   expect(linkElement).toBeInTheDocument();
 });
