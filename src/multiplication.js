d7f6a39e0980bfa8ea2164f40df696e927ec4d19
Gregory Caloin <gregchapauli@gmail.com>
2022-07-15 17:44:26 +0200

    test TDD coriigé et fonction créée

---
+++ src/multiplication.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function multiplication(a, b) {
+  return a * b;
+}
+
+export default multiplication;
--- src/multiplication.test.js
+++ src/multiplication.test.js
@@ -1,12 +1,10 @@
 import multiplication from "./multiplication";
 
 test("multiplication de 2 number", () => {
-  expect(multiplication(2, 3).toBe(6));
-  expect(multiplication(2, 3).toBeA(number));
-  expect(multiplication(2, 3).toEqual(6));
-  expect(
-    multiplication("toto", "tata").toEqual(
-      Error("number expected as parameter")
-    )
+  expect(multiplication(2, 3)).toBe(6);
+  expect(typeof multiplication(2, 3)).toBe("number");
+  expect(multiplication(2, 3)).toEqual(6);
+  expect(multiplication("toto", "tata")).toEqual(
+    Error("number expected as parameter")
   );
 });
