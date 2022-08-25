8bee03fc3058cedd1d8fd9833eb408d05853fe8d
Gregory Caloin <gregchapauli@gmail.com>
2022-08-25 09:17:32 +0200

    Initialisation du test pour le composant HidePassword

---
+++ src/hidePassword.test.js
@@ -0,0 +1,9 @@
+//1 Render du component avec un mdp <HidePassword>{mdp}</HidePassword>
+//2 Test que la valeur de mdp ne s'affiche pas à l'ecran
+//3 Simulation de click sur le libelle "afficher mdp"
+//4 Test que la valeur de mdp s'affiche bien a l'ecran
+//utilisation de '@testing-library/react'
+
+import { render, screen, fireEvent } from "@testing-library/react";
+
+test("test du rendu de affichage du mdp", () => {});
