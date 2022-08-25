3707d175ae0de168b9e445ea5eb66346faf5e391
Gregory Caloin <gregchapauli@gmail.com>
2022-08-25 08:48:11 +0200

    Tests de la fonction addition réalisés

--- src/addition.test.js
+++ src/addition.test.js
@@ -3,3 +3,12 @@
 //Le resultat doit être un number
 //Les entrées sont bien des number
 //Les parametres autres que number retournent un message d'erreur
+
+test("addition de 2 number", () => {
+  expect(addition(5, 10)).toBe(15);
+  expect(typeof addition(5, 10)).toBe("number");
+  expect(addition(5, 10)).toEqual(15);
+  expect(addition("string", "fluo")).toEqual(
+    Error("Number expected as parameter")
+  );
+});
