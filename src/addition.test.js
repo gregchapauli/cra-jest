e803b4ab4e4c3aa971eeaf55c3cf9cce340e0c47
Gregory Caloin <gregchapauli@gmail.com>
2022-08-25 08:41:24 +0200

    Spécifications du test de la fonction addition

---
+++ src/addition.test.js
@@ -0,0 +1,5 @@
+//SPECIFICATIONS
+//L'entrée des parametres 5 et 10 donne le resultat 15
+//Le resultat doit être un number
+//Les entrées sont bien des number
+//Les parametres autres que number retournent un message d'erreur
